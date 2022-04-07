d7c518461f9ff6eae3b6a0f74633a6168fa982da
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-04-06 23:04:08 -0500

    Integrate with the Front End and Refactor

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema(
   {
@@ -11,6 +12,7 @@ const PizzaSchema = new Schema(
     createdAt: {
         type: Date,
         default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
@@ -27,10 +29,16 @@ const PizzaSchema = new Schema(
 {
     toJSON: {
         virtuals: true,
+        getters: true
     },
     id: false
 }
 );
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+});
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
+router.use('/comments', commentRoutes);
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
 
