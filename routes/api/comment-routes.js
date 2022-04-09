11a79e474ad0f165e303d94b46b6d4c23a129174
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-04-09 00:40:48 -0500

    Set Up Replies

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { 
+    addComment, 
+    removeComment,
+    addReply,
+    removeReply 
+} = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
@@ -7,4 +12,12 @@ router.route('/:pizzaId').post(addComment);
 // /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
 
+// PUT route to handle addNewReply()
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment)
+
+// DELETE route to handle removeReply()
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 module.exports = router;
